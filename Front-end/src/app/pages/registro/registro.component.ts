b66819de6aefe5898b8929249dfe8f5212abc49c
maria rico <lira_384@hotmail.com>
2022-07-30 03:08:56 +0200

    Changed front end

--- Front-end/src/app/pages/registro/registro.component.ts
+++ Front-end/src/app/pages/registro/registro.component.ts
@@ -1,26 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { ApiService } from 'src/app/servicios/api/api.service';
+import { User } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.scss']
+  styleUrls: ['./registro.component.scss'],
+  providers: [ApiService],
 })
 export class RegistroComponent implements OnInit {
-  valorparametre = '';
-  usuarios = null;
+  showSuccessMessage: boolean;
+  serverErrorMessage: string;
 
-  nuevoUsuario = {
-    idUsuario: 0,
-    nombre: "",
-    correo: "",
-    contraseya: "",
-  }
+  selectUser: User = {
+    name: '',
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor(private ApiService: ApiService,private router: Router) {}
 
   ngOnInit(): void {
+    //this.resetForm();
   }
 
+
+
+  onSubmit(form:NgForm){
+    this.ApiService.posrtUser (form.value).subscribe (
+      res =>{
+        this.showSuccessMessage= true;
+        setTimeout(()=>this.showSuccessMessage= false,3000);
+        this.resetForm(form);
+        this.serverErrorMessage='';
+        this.router.navigate(['Login']);
+      }
+    )
+ }
+
+ resetForm(form?: NgForm) {
+  if (form != null) form.reset;
+  this.selectUser = {
+    name: '',
+    email: '',
+    password: '',
+  };
+  form?.resetForm();
 }
+  }
+
